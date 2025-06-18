77008e0d072f83c3b68bbc9946f021b365bd0288
azher ali <azhermurad@gmail.com>
2025-06-18 16:46:35 +0500

    openai chart model

---
+++ langchain/1.openai/chartmodel.py
@@ -0,0 +1,18 @@
+import getpass
+import os
+from langchain_openai import ChatOpenAI
+
+if not os.environ.get("OPENAI_API_KEY"):
+    os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
+
+
+print(os.environ.get("OPENAI_API_KEY"))
+
+llm = ChatOpenAI(
+    model="gpt-4o",
+    temperature=0
+)
+
+
+ai_msg = llm.invoke("what is deep learning?")
+ai_msg
